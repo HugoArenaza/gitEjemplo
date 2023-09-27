c5df58f5e5809d0bd8c4edf99c703dd787abb8e5
hugo.arenaza <hugo.arenaza@E108L4.opendeusto.es>
2023-09-27 10:53:10 +0200

    Integracion funcion de suma

--- GitEjemplo/src/HolaMundo.java
+++ GitEjemplo/src/HolaMundo.java
@@ -1,11 +1,17 @@
 
 public class HolaMundo {
+	
 public static void main(String[] args) {
 	System.out.println("Que onda tete!!!!!!!!!");
 	System.out.println("Arreglado");
+	 int a=6;
+	 int b=7;
+	 int c= sumar(a, b);
+	 System.out.println("La suma de "+ a+" + " + b +" es igual a "+ c);
+	 
 	}
 
-	public int sumar(int a,int b){
+	public static int sumar(int a, int b){
 	return a + b;
 	}
 }
